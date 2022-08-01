2d90a5c76197653c6c764b3c1e7a9b45775312f0
Aakash Haran <aakash.haran01@gmail.com>
2022-07-31 21:37:16 -0500

    added resume preview

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {Typewriter as T} from 'typewriter-effect/dist/core';
+import { Typewriter as T } from 'typewriter-effect/dist/core';
 import Typewriter from 'typewriter-effect';
 import LogoRow from './Contact/LogoRow';
 import { Nav } from 'react-bootstrap';
@@ -8,22 +8,22 @@ import { Link } from "react-scroll";
 
 export default function Home() {
     const [isShown, setShown] = useState(false);
-    const [aboutText, setAboutText] =  useState("Hi, my name is Aakash and I'm...")
+    const [aboutText, setAboutText] = useState("Hi, my name is Aakash and I'm...")
 
     function getLink() {
         if (isShown) {
-            return <Link style={{paddingTop: '2%'}}
-            href='#about_me'
-            to='about_me'
-            activeClass="active"
-            className="nav-link"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-        >
-            <button class='btn btn-primary'>Learn More</button>
-        </Link>
+            return <Link style={{ paddingTop: '2%' }}
+                href='#about_me'
+                to='about_me'
+                activeClass="active"
+                className="nav-link"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+            >
+                <button class='btn btn-primary'>Learn More</button>
+            </Link>
         }
     }
 
@@ -42,11 +42,12 @@ export default function Home() {
         ''];
 
     return (
-        <div id='home' style={{ height: '100vh', textAlign: 'center'}}>
+        <div id='home' style={{ height: '100vh', textAlign: 'center' }}>
             {/* <div id='typewriter' style={{paddingTop: '3%'}}>
 
             </div> */}
             <h1 style={{ paddingTop: '20%' }}>{aboutText}</h1>
+
 
             <Typewriter
                 options={{
